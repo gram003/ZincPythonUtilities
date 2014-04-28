74ca6318af376f71d4a5b1dffa6dd0f916c861cf
Glenn Ramsey <g.ramsey@auckland.ac.nz>
2014-04-28 16:59:30 +1200

    Update tests

--- test/test_fitter.py
+++ test/test_fitter.py
@@ -30,8 +30,16 @@ class Test(unittest.TestCase):
         # for a directory context manager see
         # http://stackoverflow.com/questions/431684/how-do-i-cd-in-python
         f = self.fitter
+        initial = mesh.nodes_to_list(self.context)
         f.register_automatic()
+        registered = mesh.nodes_to_list(self.context)
+        print initial[0][0], registered[0][0]
+        a = np.array(initial)
+        b = np.array(registered)
+        self.assertFalse(np.allclose(a,b), "Initial and registered arrays are equal")
+        
 
+    #@unittest.skip("")
     def testMirror(self):
         f = self.fitter
         # Get the list of nodes
@@ -39,11 +47,12 @@ class Test(unittest.TestCase):
         f.data_mirror(0) # mirror in x axis
         mirrored = mesh.data_to_list(self.context)
         #print initial[0][0], mirrored[0][0]
-        self.assertTrue(initial[0][0] == -mirrored[0][0])
+        # can't do this because it now mirrors about a plane through the centroid
+        #self.assertTrue(initial[0][0] == -mirrored[0][0])
         a = np.array(initial)
         b = np.array(mirrored)
-        #print a
-        #print b
+        print a
+        print b
         self.assertFalse(np.allclose(a,b), "Initial and mirrored arrays are equal")
 
 
