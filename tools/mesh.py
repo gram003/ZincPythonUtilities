f3ee8f970098f4d30061b5a5740de7f9b853984b
Glenn Ramsey <g.ramsey@auckland.ac.nz>
2014-04-30 10:06:14 +1200

    Use a context manager for the field module

--- tools/mesh.py
+++ tools/mesh.py
@@ -7,6 +7,8 @@ from opencmiss.zinc.element import Element, Elementbasis
 
 _defaultGraphicsCreated = False
 
+from tools.utilities import get_field_module
+
 # for diagnostics
 def funcname():
     return sys._getframe(1).f_code.co_name
@@ -29,40 +31,38 @@ def _coordinate_field(ctxt, coordinate_set, nodeset_type, coordinate_field_name,
     
     # Get the field module for root region, with which we  shall create a 
     # finite element coordinate field.
-    field_module = default_region.getFieldmodule()
+    with get_field_module(default_region) as field_module:
     
-    field_module.beginChange()
-
-    finite_element_field = field_module.createFieldFiniteElement(coord_count)
-    finite_element_field.setName(coordinate_field_name)
+        finite_element_field = field_module.createFieldFiniteElement(coord_count)
+        finite_element_field.setName(coordinate_field_name)
+        
+        # Find a special node set named 'nodes' or 'datapoints'
+        nodeset = field_module.findNodesetByName(nodeset_type)
+        node_template = nodeset.createNodetemplate()
     
-    # Find a special node set named 'nodes' or 'datapoints'
-    nodeset = field_module.findNodesetByName(nodeset_type)
-    node_template = nodeset.createNodetemplate()
-
-    # Set the finite element coordinate field for the nodes to use
-    node_template.defineField(finite_element_field)
-    field_cache = field_module.createFieldcache()
-    
-    node_id = 1
-    # Create nodes and add to field cache
-    for coords in coordinate_set:
-        if not merge:
-            node = nodeset.createNode(-1, node_template)
-            # Set the node coordinates, first set the field cache to use the current node
-            field_cache.setNode(node)
-            # Pass in floats as an array
-            finite_element_field.assignReal(field_cache, coords)
-        else:
-            node = nodeset.findNodeByIdentifier(node_id)
-            field_cache.setNode(node)
-            node.merge(node_template)
-            finite_element_field.assignReal(field_cache, coords)
-            node_id += 1
+        # Set the finite element coordinate field for the nodes to use
+        node_template.defineField(finite_element_field)
+        field_cache = field_module.createFieldcache()
+        
+        node_id = 1
+        # Create nodes and add to field cache
+        for coords in coordinate_set:
+            if not merge:
+                node = nodeset.createNode(-1, node_template)
+                # Set the node coordinates, first set the field cache to use the current node
+                field_cache.setNode(node)
+                # Pass in floats as an array
+                finite_element_field.assignReal(field_cache, coords)
+            else:
+                node = nodeset.findNodeByIdentifier(node_id)
+                field_cache.setNode(node)
+                node.merge(node_template)
+                finite_element_field.assignReal(field_cache, coords)
+                node_id += 1
+                
+        finite_element_field.setTypeCoordinate(True)
             
-    finite_element_field.setTypeCoordinate(True)
-    
-    return nodeset
+        return nodeset
 
 # map shape type to mesh order
 _shape_type_map = {1: Element.SHAPE_TYPE_LINE,
@@ -85,67 +85,64 @@ def linear_mesh(ctxt, node_coordinate_set, element_set, **kwargs):
         raise RuntimeError("Empty node list") 
 
     default_region = ctxt.getDefaultRegion()
-    field_module = default_region.getFieldmodule()
+    with get_field_module(default_region) as field_module:
 
-    # Parse kwargs    
-    coordinate_field_name = kwargs.get('coordinate_field_name', 'coordinates')
-    merge = kwargs.get('merge', False)
-
-    nodeset = _coordinate_field(ctxt, node_coordinate_set, 'nodes', coordinate_field_name, merge)
-
-    # Create and configure an element template for the appropriate mesh type.
-    element_node_count = len(element_set[0])
-
-    order = int(math.log(element_node_count, 2))
-    #if __debug__: print "mesh order", order        
-
-    field_module.beginChange()
-
-    mesh = field_module.findMeshByDimension(order)
-    element_template = mesh.createElementtemplate()
+        # Parse kwargs    
+        coordinate_field_name = kwargs.get('coordinate_field_name', 'coordinates')
+        merge = kwargs.get('merge', False)
     
+        nodeset = _coordinate_field(ctxt, node_coordinate_set, 'nodes', coordinate_field_name, merge)
     
-    element_template.setElementShapeType(_shape_type_map[order])
-    element_template.setNumberOfNodes(element_node_count)
+        # Create and configure an element template for the appropriate mesh type.
+        element_node_count = len(element_set[0])
     
-    # Specify the dimension and the interpolation function for the element basis function
-    linear_basis = field_module.createElementbasis(
-           order,
-           Elementbasis.FUNCTION_TYPE_LINEAR_LAGRANGE)
+        order = int(math.log(element_node_count, 2))
+        #if __debug__: print "mesh order", order        
+        
+        mesh = field_module.findMeshByDimension(order)
+        element_template = mesh.createElementtemplate()
+        
+        
+        element_template.setElementShapeType(_shape_type_map[order])
+        element_template.setNumberOfNodes(element_node_count)
+        
+        # Specify the dimension and the interpolation function for the element basis function
+        linear_basis = field_module.createElementbasis(
+               order,
+               Elementbasis.FUNCTION_TYPE_LINEAR_LAGRANGE)
+        
+        # the indices of the nodes in the node template we want to use.
+        local_indices = [x for x in xrange(1, element_node_count+1)]
+        
+        # Define a nodally interpolated element field or field component in the
+        # element_template
+        finite_element_field = field_module.findFieldByName(coordinate_field_name)
+        element_template.defineFieldSimpleNodal(finite_element_field,
+                                                -1,
+                                                linear_basis,
+                                                local_indices)
     
-    # the indices of the nodes in the node template we want to use.
-    local_indices = [x for x in xrange(1, element_node_count+1)]
+        # create the elements
+        element_id = 1
+        for node_indices in element_set:
+            def _populateTemplate():
+                for i, node_idx in enumerate(node_indices):
+                    node = nodeset.findNodeByIdentifier(node_idx)
+                    element_template.setNode(i + 1, node)
+                
+            if not merge:
+                _populateTemplate()
+                mesh.defineElement(-1, element_template)
+            else:
+                element = mesh.findElementByIdentifier(element_id)
+                _populateTemplate()
+                
+                element.merge(element_template)
+                element_id += 1
+                
     
-    # Define a nodally interpolated element field or field component in the
-    # element_template
-    finite_element_field = field_module.findFieldByName(coordinate_field_name)
-    element_template.defineFieldSimpleNodal(finite_element_field,
-                                            -1,
-                                            linear_basis,
-                                            local_indices)
-
-    # create the elements
-    element_id = 1
-    for node_indices in element_set:
-        def _populateTemplate():
-            for i, node_idx in enumerate(node_indices):
-                node = nodeset.findNodeByIdentifier(node_idx)
-                element_template.setNode(i + 1, node)
-            
-        if not merge:
-            _populateTemplate()
-            mesh.defineElement(-1, element_template)
-        else:
-            element = mesh.findElementByIdentifier(element_id)
-            _populateTemplate()
-            
-            element.merge(element_template)
-            element_id += 1
-            
-
-    finite_element_field.setTypeCoordinate(True) 
-    field_module.defineAllFaces() 
-    field_module.endChange()
+        finite_element_field.setTypeCoordinate(True) 
+        field_module.defineAllFaces() 
     
 def data_points(ctxt, coordinate_set, field_name='data_coordinates'):
     
@@ -205,19 +202,17 @@ def _list_to_nodes(ctxt, coordinate_set, nodesetName, coordFieldName='coordinate
     field = fm.findFieldByName(coordFieldName)
     
     # Update nodes with new coordinates 
-    fm.beginChange()
-
-    cache = fm.createFieldcache()
-    node_id = 1
-    for coords in coordinate_set:
-        node = sNodes.findNodeByIdentifier(node_id)
-        cache.setNode(node)
-        field.assignReal(cache, coords)
-        node_id += 1
-            
-    fm.endChange()
+    with get_field_module(region) as fm:
     
-
+        cache = fm.createFieldcache()
+        node_id = 1
+        for coords in coordinate_set:
+            node = sNodes.findNodeByIdentifier(node_id)
+            cache.setNode(node)
+            result = field.assignReal(cache, coords)
+            node_id += 1
+            
+    
 def nodes_to_list(ctxt, numValues=3, coordFieldName='coordinates'):
     """
     Return all nodes as a Python list
---
+++ tools/utilities.py
@@ -0,0 +1,42 @@
+'''
+Created on 30/04/2014
+
+@author: glenn
+'''
+
+import contextlib
+
+@contextlib.contextmanager
+def get_scene(region):
+    """
+    A context manager for the Scene class. When used with the Python
+    with statement ensures that beginChange() and endChange() are called.
+    Usage:
+        with get_scene(region) as scene:
+            scene.some_method()
+    """
+    scene = region.getScene()
+    scene.beginChange()
+    try:
+        yield scene
+    finally:
+        scene.endChange()
+    return
+
+
+@contextlib.contextmanager
+def get_field_module(region):
+    """
+    A context manager for the Fieldmodule class. When used with the Python
+    with statement ensures that beginChange() and endChange() are called.
+    Usage:
+        with get_field_module(region) as field_module:
+            field_module.some_method()
+    """
+    fm = region.getFieldmodule()
+    fm.beginChange()
+    try:
+        yield fm
+    finally:
+        fm.endChange()
+    return
