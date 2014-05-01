d5b3a3f7220a48045827a640b9d588d1c74c46d8
Glenn Ramsey <g.ramsey@auckland.ac.nz>
2014-05-01 15:13:06 +1200

    Experiment with setting the Zinc context

--- enaml_zinc_widget.py
+++ enaml_zinc_widget.py
@@ -1,18 +1,28 @@
 
 from zincwidget import ZincWidget
-from opencmiss.zinc.context import Context
 
-from atom.api import set_default
+from atom.api import set_default, Typed
 from enaml.widgets.api import RawWidget
+from enaml.core.api import d_
+
+#import atom.api
+from opencmiss.zinc.context import Context
 
 class EZincWidget(RawWidget):
     hug_width = set_default('ignore')
     hug_height = set_default('ignore')
+    context = d_(Typed(Context))
+    #widget = d_(Typed(ZincWidget))
     
-    def create_widget(self, parent):
+#     def __init__(self, parent):
+#         super(EZincWidget, self).__init__(parent)
+    
+    def set_zinc_context(self, context):
+        #self._context.s
+        print "set_zinc_context", context
+        self.context = context
         
-        c = Context("EZincWidget")
-
+    def create_widget(self, parent):
         widget = ZincWidget(parent)
-        widget.setContext(c)
+        widget.setContext(self.context)
         return widget
