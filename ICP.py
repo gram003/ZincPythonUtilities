e78932631ced7d2eb428b1fed7ad78834b6aa3c2
Glenn Ramsey <g.ramsey@auckland.ac.nz>
2014-05-01 15:10:38 +1200

    Add options for rigid fitting

--- ICP.py
+++ ICP.py
@@ -63,6 +63,32 @@ def translateRigid3D( x, t ):
 #    T[:3,:3] = scipy.dot( scipy.dot( Rx,Ry ),Rz )
     return scipy.dot( T, X )[:3,:].T
 
+def rotateRigid3D( x, t ):
+    """ applies a rigid rotation to list of points x.
+    t = (tx,ty,tz,rx,ry,rz)
+    """
+    X = scipy.vstack( (x.T, scipy.ones(x.shape[0]) ) )
+    T = scipy.array([[1.0, 0.0, 0.0, 0.0],\
+               [0.0, 1.0, 0.0, 0.0],\
+               [0.0, 0.0, 1.0, 0.0],\
+               [1.0, 1.0, 1.0, 1.0]])
+        
+    Rx = scipy.array( [[1.0, 0.0, 0.0],\
+                 [0.0, scipy.cos(t[3]), -scipy.sin(t[3])],\
+                 [0.0, scipy.sin(t[3]),  scipy.cos(t[3])]] )
+    
+    Ry = scipy.array( [[scipy.cos(t[4]), 0.0, scipy.sin(t[4])],\
+                 [0.0, 1.0, 0.0],\
+                 [-scipy.sin(t[4]), 0.0, scipy.cos(t[4])]] )
+    
+    Rz = scipy.array( [[scipy.cos(t[5]), -scipy.sin(t[5]), 0.0],\
+                 [scipy.sin(t[5]), scipy.cos(t[5]), 0.0],\
+                 [0.0, 0.0, 1.0]] )
+    
+    T[:3,:3] = scipy.dot( scipy.dot( Rx,Ry ),Rz )
+    return scipy.dot( T, X )[:3,:].T
+
+
 def transformScale3D( x, S ):
     """ applies scaling to a list of points x. S = (sx,sy,sz)
     """
@@ -89,12 +115,12 @@ def matchCentroid(x, S):
 
 def boundingBox(x):
     """Compute the bounding box of points x"""
-    min = np.min(x, axis=0)
-    max = np.max(x, axis=0)
-    return min, max
+    min_ = np.min(x, axis=0)
+    max_ = np.max(x, axis=0)
+    return min_, max_
 
 #======================================================================#
-def fitDataRigidScaleEPDP( X, data, xtol=1e-5, maxfev=0 ):
+def fitDataEPDP( X, data, xtol=1e-5, maxfev=0, translate=True, rotate=True, scale=True):
     """ fit list of points X to list of points data by minimising
     least squares distance between each point in X and closest neighbour
     in data. Rigid transformation plus scaling.
@@ -103,21 +129,145 @@ def fitDataRigidScaleEPDP( X, data, xtol=1e-5, maxfev=0 ):
     tOpt: optimised transformations [tx, ty, tz, rx, ry, rz, s]
     XOpt: transformed points
     """
+
+    dataTree = cKDTree( data )
+    X = scipy.array(X)
     
+    def obj( t ):
+        # FIXME: there should be a way to compose this as a sequence of
+        # numpy arrays, which would probably be more efficient
+        # See Mithraratne 2006 Customisation of anatomically based musculoskeletal structures
+        
+        if translate and not rotate:
+            xR = translateRigid3D( X, t[:6] )
+        elif not translate and rotate:
+            xR = rotateRigid3D( X, t[:6] )
+        elif translate and rotate:
+            xR = transformRigid3D( X, t[:6] )
+        elif not translate and not rotate:
+            xR = X
+            
+        if scale:
+            xRS = transformScale3D( xR, scipy.ones(3)*t[6] )
+        else:
+            xRS = xR
+
+        d = dataTree.query( list(xRS) )[0]
+        #~ print d.mean()
+        return d*d        
+    
+    t0 = scipy.array([0.0,0.0,0.0,0.0,0.0,0.0,1.0])
+    tOpt = leastsq( obj, t0, xtol=xtol, maxfev=maxfev )[0]
+    
+    if translate and not rotate:
+        XOpt = translateRigid3D( X, tOpt[:6] )
+    elif not translate and rotate:
+        XOpt = rotateRigid3D( X, tOpt[:6] )
+    elif translate and rotate:
+        XOpt = transformRigid3D( X, tOpt[:6] )
+    elif not translate and not rotate:
+        XOpt = X
+
+    if scale:
+        XOpt = transformScale3D( XOpt, tOpt[6:] )
+    
+    return tOpt, XOpt
+
+def fitDataEPDP2( X, data, xtol=1e-5, maxfev=0, translate=True, rotate=True, scale=True):
+    """ fit list of points X to list of points data by minimising
+    least squares distance between each point in X and closest neighbour
+    in data. Rigid transformation plus scaling in three directions.
+    
+    return:
+    tOpt: optimised transformations [tx, ty, tz, rx, ry, rz, sx, sy, sz]
+    XOpt: transformed points
+    
+    FIXME: this is not working yet
+    """
+
+    dataTree = cKDTree( data )
+    X = scipy.array(X)
+    
+    def transform(X, t):
+        # Rotation matrix
+        Rx = scipy.array( [[1.0, 0.0, 0.0],\
+                     [0.0, scipy.cos(t[3]), -scipy.sin(t[3])],\
+                     [0.0, scipy.sin(t[3]),  scipy.cos(t[3])]] )
+        
+        Ry = scipy.array( [[scipy.cos(t[4]), 0.0, scipy.sin(t[4])],\
+                     [0.0, 1.0, 0.0],\
+                     [-scipy.sin(t[4]), 0.0, scipy.cos(t[4])]] )
+        
+        Rz = scipy.array( [[scipy.cos(t[5]), -scipy.sin(t[5]), 0.0],\
+                     [scipy.sin(t[5]), scipy.cos(t[5]), 0.0],\
+                     [0.0, 0.0, 1.0]] )
+    
+        R = scipy.dot( scipy.dot( Rx,Ry ),Rz )
+        
+        # Scaling matrix
+        s = scipy.array(
+                        [[t[6], 0.0, 0.0],\
+                         [0.0, t[7], 0.0],\
+                         [0.0, 0.0, t[8]]])
+        
+        # Translation matrix
+        dX = scipy.array([t[0], t[1], t[2]])
+        
+        xR = ((s.dot(R)).dot(X.T)).T - dX
+        print X[0]
+        print ((s.dot(R)).dot(X.T)).T[0]
+        print xR[0]
+        
+        return xR
+    
+    def obj( t ):
+        # FIXME: there should be a way to compose this as a sequence of
+        # numpy arrays, which would probably be more efficient
+        # See Mithraratne 2006 Customisation of anatomically based musculoskeletal structures
+        
+        xR = transform(X, t)
+        
+        d = dataTree.query( list(xR) )[0]
+        #~ print d.mean()
+        return d*d        
+    
+    t0 = scipy.array([0.0,0.0,0.0,0.0,0.0,0.0,1.0,1.0,1.0])
+    tOpt = leastsq( obj, t0, xtol=xtol, maxfev=maxfev )[0]
+    
+    XOpt = transform(X, tOpt)
+        
+    return tOpt, XOpt
+
+
+#======================================================================#
+def fitDataRigidScaleEPDP( X, data, xtol=1e-5, maxfev=0):
+    """ fit list of points X to list of points data by minimising
+    least squares distance between each point in X and closest neighbour
+    in data. Rigid transformation plus scaling.
+    
+    return:
+    tOpt: optimised transformations [tx, ty, tz, rx, ry, rz, s]
+    XOpt: transformed points
+    """
+    scale = False
     dataTree = cKDTree( data )
     X = scipy.array(X)
     
     def obj( t ):
         xR = transformRigid3D( X, t[:6] )
-        xRS = transformScale3D( xR, scipy.ones(3)*t[6] )
+        if scale:
+            xRS = transformScale3D( xR, scipy.ones(3)*t[6] )
+        else:
+            xRS = xR
         d = dataTree.query( list(xRS) )[0]
-        #~ print d.mean()
+        #print d.mean()
         return d*d
     
     t0 = scipy.array([0.0,0.0,0.0,0.0,0.0,0.0,1.0])
     tOpt = leastsq( obj, t0, xtol=xtol, maxfev=maxfev )[0]
     XOpt = transformRigid3D( X, tOpt[:6] )
-    XOpt = transformScale3D( XOpt, tOpt[6:] )
+    if scale:
+        XOpt = transformScale3D( XOpt, tOpt[6:] )
     
     return tOpt, XOpt
 
@@ -135,8 +285,8 @@ def fitDataRigidEPDP( X, data, xtol=1e-5, maxfev=0 ):
     X = scipy.array(X)
     
     def obj( t ):
-        #xR = transformRigid3D( X, t[:6] )
-        xR = translateRigid3D( X, t[:6] )
+        xR = transformRigid3D( X, t[:6] )
+        #xR = translateRigid3D( X, t[:6] )
         #xRS = transformScale3D( xR, scipy.ones(3)*t[6] )
         #d = dataTree.query( list(xRS) )[0]
         d = dataTree.query( list(xR) )[0]
