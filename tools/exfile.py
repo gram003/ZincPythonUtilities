c02bacaf181f03aeb4970998ed1d89f697917b78
Glenn Ramsey <g.ramsey@auckland.ac.nz>
2014-09-02 10:31:37 +1200

    Remove self

--- tools/exfile.py
+++ tools/exfile.py
@@ -1,6 +1,6 @@
 from opencmiss.zinc.field import Field
 
-def read_ex(self, region, path):
+def read_ex(region, path):
     """
     Read an ex* format file
     """
@@ -8,34 +8,34 @@ def read_ex(self, region, path):
     sir.createStreamresourceFile(path)
     region.read(sir)
 
-def _write_ex(self, region, path, domainTypes):
+def _write_ex(region, path, domainTypes):
     """
     Write an ex* format file
     """
     sir = region.createStreaminformationRegion()
-    sir.createStreamresourceFile(path)
-    sir.setResourceDomainTypes(domainTypes) 
-    region.read(sir)
+    res = sir.createStreamresourceFile(path)
+    sir.setResourceDomainTypes(res, domainTypes) 
+    region.write(sir)
 
-def write_exmesh(self, region, path):
+def write_exmesh(region, path):
     """
     Write the element and node data as ex* format file
     """
     # FIXME: check that the filename suffix is .exmesh
-    domainTypes = Field.DOMAIN_TYPE_NODES |
-        DomainType.DOMAIN_TYPE_MESH1D |
-        DomainType.DOMAIN_TYPE_MESH2D |
-        DomainType.DOMAIN_TYPE_MESH3D
-    _write_ex(self, region, path, domainTypes)
-
-def write_exnode(self, region, path):
-    _write_ex(self, region, path, Field.DOMAIN_TYPE_NODES)
-
-def write_exdata(self, region, path):
-    _write_ex(self, region, path, Field.DOMAIN_TYPE_DATAPOINTS)
-
-def write_exelem(self, region, path):
-    domainTypes = Field.DOMAIN_TYPE_MESH1D |
-        Field.DOMAIN_TYPE_MESH2D |
+    domainTypes = Field.DOMAIN_TYPE_NODES |\
+        Field.DOMAIN_TYPE_MESH1D |\
+        Field.DOMAIN_TYPE_MESH2D |\
         Field.DOMAIN_TYPE_MESH3D
-    _write_ex(self, region, path, domainTypes):
+    _write_ex(region, path, domainTypes)
+
+def write_exnode(region, path):
+    _write_ex(region, path, Field.DOMAIN_TYPE_NODES)
+
+def write_exdata(region, path):
+    _write_ex(region, path, Field.DOMAIN_TYPE_DATAPOINTS)
+
+def write_exelem(region, path):
+    domainTypes = Field.DOMAIN_TYPE_MESH1D |\
+        Field.DOMAIN_TYPE_MESH2D |\
+        Field.DOMAIN_TYPE_MESH3D
+    _write_ex(region, path, domainTypes)
